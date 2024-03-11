cde1b5749bd6c3698c92bf92ac265b975c0af770
xDealer <161089629+xDealer@users.noreply.github.com>
2024-03-11 23:16:22 +0300

    Update ca.sh

--- ca.sh
+++ ca.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# Install Docker
-apt install docker.io -y
-
 # Pull the Docker image
 docker pull traffmonetizer/cli_v2
 
 # Run the Docker container with the specified settings
-docker run -i --name tm traffmonetizer/cli_v2 start accept --token MYRW5FG2ioiBE7IormZYaxTOlSONHNq0slsMoLoWQE8=
+docker run -d --restart=always --name tm traffmonetizer/cli_v2 start accept --token MYRW5FG2ioiBE7IormZYaxTOlSONHNq0slsMoLoWQE8=
